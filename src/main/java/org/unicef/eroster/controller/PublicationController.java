2e7f0080789b454b8f31c5d1d0f0c5c9178edc6d
nathanbaleeta <nbaleeta@gmail.com>
2020-06-01 12:29:48 +0300

    Added api endpoint for publications with getPublicationbyId and deletePublicationbyId methods for CRUD fucntionality

--- src/main/java/org/unicef/eroster/controller/PublicationController.java
+++ src/main/java/org/unicef/eroster/controller/PublicationController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.unicef.eroster.exception.RecordNotFoundException;
 import org.unicef.eroster.model.Publication;
+import org.unicef.eroster.model.Reference;
 import org.unicef.eroster.service.PublicationService;
 
 import java.util.List;
@@ -24,5 +24,19 @@ public class PublicationController {
         List<Publication> list = service.getAllPublications();
 
         return new ResponseEntity<List<Publication>>(list, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Publication> getPublicationById(@PathVariable("id") Long id)
+            throws RecordNotFoundException {
+        Publication entity = service.getPublicationById(id);
+
+        return new ResponseEntity<Publication>(entity, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deletePublicationById(@PathVariable("id") Long id) throws RecordNotFoundException {
+        service.deletePublicationById(id);
+        return HttpStatus.FORBIDDEN;
     }
 }
--- src/main/java/org/unicef/eroster/service/PublicationService.java
+++ src/main/java/org/unicef/eroster/service/PublicationService.java
@@ -2,11 +2,14 @@ package org.unicef.eroster.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.unicef.eroster.exception.RecordNotFoundException;
 import org.unicef.eroster.model.Publication;
+import org.unicef.eroster.model.Reference;
 import org.unicef.eroster.repository.PublicationRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PublicationService {
@@ -21,6 +24,29 @@ public class PublicationService {
             return publicationList;
         } else {
             return new ArrayList<Publication>();
+        }
+    }
+
+    public Publication getPublicationById(Long id) throws RecordNotFoundException
+    {
+        Optional<Publication> publication = repository.findById(id);
+
+        if(publication.isPresent()) {
+            return publication.get();
+        } else {
+            throw new RecordNotFoundException("No publication record exists for given id");
+        }
+    }
+
+    public void deletePublicationById(Long id) throws RecordNotFoundException
+    {
+        Optional<Publication> publication = repository.findById(id);
+
+        if(publication.isPresent())
+        {
+            repository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("No publication record exists for given id");
         }
     }
 }
